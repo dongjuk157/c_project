b63c32a625649f3d53595fcd2a3ae62c78a0328d
SSong <sonk55@naver.com>
2022-03-01 22:21:15 +0900

    feat : 파일 읽어서 저장 기능 추가

--- src/info.c
+++ src/info.c
@@ -1,5 +1,48 @@
 #include "info.h"
+#include "manage.h"
+#include <stdio.h>
 
 void printInfo(Info info){
     
+}
+
+int readUserData(LPHASH *hash){
+    FILE *fp = fopen(USER_DATA_FILE_PATH, "rb");
+    USER_INFO *tmp_user;
+    while (1){
+        tmp_user = (USER_INFO *)malloc(sizeof(USER_INFO));
+        fread(tmp_user, sizeof(USER_INFO), 1, fp);
+        if (feof(fp)) 
+            break;
+        hashSetValue(*hash, tmp_user->car_num, tmp_user);
+    }
+    fclose(fp);
+}
+
+int readParkingLot(LinkedList* list){
+    FILE* fp = fopen(PARKINGLOT_SETTINGS_FILE_PATH, "rb");
+    PARK *tmp_park;
+    while (1){
+        tmp_park = (PARK *)malloc(sizeof(PARK));
+        fread(tmp_park, sizeof(PARK), 1, fp);
+        if (feof(fp)) 
+            break;
+        
+        list_push_back(list, tmp_park);
+    }
+    fclose(fp);
+}
+
+int readCurrentData(LinkedList* list){
+    // 파일 current.dat 읽어서 리스트 생성
+    FILE* fp = fopen(CURRENT_DATA_FILE_PATH, "rb");
+    CAR_INFO *tmp_car_info;
+    while (1){
+        tmp_car_info = (CAR_INFO *)malloc(sizeof(CAR_INFO));
+        fread(tmp_car_info, sizeof(CAR_INFO), 1, fp);
+        if (feof(fp)) 
+            break;
+        list_push_back(&list, tmp_car_info);
+    }
+    fclose(fp);
 }
