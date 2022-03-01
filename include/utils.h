54757b6b4a718b08a78a59e60c5f5021f330963f
SSong <sonk55@naver.com>
2022-03-01 18:22:40 +0900

    feat : 요금계산 함수 추가 calulate_fee

--- include/utils.h
+++ include/utils.h
@@ -3,9 +3,12 @@
 #define False 0
 #define True 1
 typedef char bool;
-
 void gotoxy(int x, int y);
 int min(int a, int b);
 // int kbhit(void);
 int getch(void);
+int to_day(int y, int m, int d);
+int diff(int y1, int m1, int d1, int y2, int m2, int d2);
+int calulate_fee(char* inDateTime, char* outDateTime);
+
 #endif // __UTILS_H__
